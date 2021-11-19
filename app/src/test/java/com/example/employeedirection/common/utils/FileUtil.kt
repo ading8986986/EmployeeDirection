6c2b1681eb9f203e3926885f3bc3034df4b93e29
Ding Ting <ting_ding_mail@163.com>
2021-11-18 19:15:55 -0500

    delete useless files

--- app/src/test/java/com/example/employeedirection/common/utils/FileUtil.kt
+++ app/src/test/java/com/example/employeedirection/common/utils/FileUtil.kt
@@ -5,8 +5,4 @@ import java.io.File
 object FileUtil {
     fun readFileAsTextUsingInputStream(fileName: String)
             = File(fileName).inputStream().readBytes().toString(Charsets.UTF_8)
-
-//    val classLoader = obj.javaClass.classLoader
-//    val resource = classLoader.getResource(fileName)
-//    return File(resource.path)
 }
